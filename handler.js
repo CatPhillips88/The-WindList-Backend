ca208eb93837cf9c69e4064d0f56bba0c0ed2c58
Catherine <catherineep4189@gmail.com>
2020-02-26 18:48:01 +0000

    API practice

--- handler.js
+++ handler.js
@@ -5,8 +5,22 @@ const express = require('express')
 const app = express();
 
 app.get('/tasks', function (req, res) {
-  res.send('Hi Cat! This is a test!');
+  res.json({
+    message: 'You got the API to work!'
+  })
 })
 
 module.exports.tasks = serverless(app);
 
+// module.exports.task => async event => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//      {
+//        message: 'This API works'
+//      },
+//      null,
+//      2
+//     ),
+//   };
+// }

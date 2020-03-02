984168333963e962f746b2ed33e4e6b9113c213c
Catherine <catherineep4189@gmail.com>
2020-03-02 18:40:41 +0000

    get request method works in rendering database tasks - hidden connections

--- handler.js
+++ handler.js
@@ -7,35 +7,30 @@ const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host     : 'example.org',
-  user     : 'bob',
-  password : 'secret',
-  database : 'my_db'
+  host     : 'XXXX',
+  user     : 'XXXX',
+  password : 'XXXX',
+  database : 'XXXX'
 });
 
 // RETRIEVE TASKS
 app.get('/tasks', function (req, res) {
-  const taskItems = [
-    {
-      id: uuidv4(),
-      content: 'buy travel insurance',
-      completed: false
-    },
-    {
-      id: uuidv4(),
-      content: 'train to gatwick @ 5am',
-      completed: false
-    },
-    {
-      id: uuidv4(),
-      content: 'don\'t forget passport!',
-      completed: true
+  
+  connection.query('SELECT * FROM `tasks` WHERE `user_id` = 1', function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    if(error) {
+      console.error("There is an error with query when fetching tasks", error);
+      res.status(500).json({errorMessage: error});
     }
-  ]
-
-  res.json({
-    tasks: taskItems
+    else {
+      // Query was successful
+      res.json({tasks: results});
+    }
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
   });
+
+  
 });
 
 // CREATING TASKS
